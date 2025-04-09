25ef8f4f80627359353db41456f3c2f6c0fbaa96
General-Seagull <61531870+zhouzxx@users.noreply.github.com>
2025-04-10 01:29:35 +0800

    Update typable-text.js

--- src/wxcomponents/towxml/typable-text/typable-text.js
+++ src/wxcomponents/towxml/typable-text/typable-text.js
@@ -19,14 +19,15 @@ Component({
         textRenderCb
       } = require("./text-cb");
       this.showText = this.data.text
-      this.setData({ showText: this.data.text });
+      this.setData({ showText: this.showText });
+      const _this = this
       // console.log("文字组件初始化完成")
       textRenderCb.value = (newText, curShowText) => {
-        const tmpText = this.data.text + newText
+        const tmpText = _this.showText + newText
         //这个if是防止后面的文本错误地添加到前面地文本组件实例上
         if (curShowText.length === tmpText.length && curShowText === tmpText) {
-          this.data.text = tmpText
-          this.setData({ showText: this.data.text });
+          _this.showText = tmpText
+          _this.setData({ showText: _this.showText });
         }
       }
     },
