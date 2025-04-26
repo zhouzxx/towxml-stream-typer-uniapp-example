babdcc1aa714beda2f729d3862ccea0f071b3fbb
“zhouzxx” <3423729430@qq.com>
2025-04-26 23:56:02 +0800

    优化

---
+++ src/pages/index/chat.vue
@@ -0,0 +1,64 @@
+<template>
+  <view v-for="msgItem in messages" :key="msgItem.id" class="item">
+    <view
+      v-if="msgItem.type == answerType"
+      class="answer-item"
+      :data-towxmlid="msgItem.id"
+    >
+      <towxml :towxmlId="msgItem.id" :speed="speed" @finish="finish" />
+    </view>
+    <view v-if="msgItem.type == questionType" class="question">
+      <view class="question-item">{{ msgItem.content }}</view>
+    </view>
+  </view>
+</template>
+
+<script>
+//看到这个组件的时候你可能会有疑问，为什么要多封装一个chat组件,里面只有一个v-for,而不直接把v-for写在index组件中
+//原因就是我要把渲染聊天记录的v-for 与 包裹它的scroll-view之间进行组件隔离，因为你可能会在打字的过程中要求时刻滚动到底部，实现的方式有改变scrollTop或者scroll-into-view等
+//无论哪一种方式都是要更新scroll-view组件上的属性，就会导致scroll-view所在的组件更新，更新意味着diff scroll-view所在组件的新老dom树等，由于微信小程序是组件级更新，如果你把往v-for和scroll-view写在一个组件里，v-for中的内容也会跟着diff而消耗性能，经过我的测试,随着聊天内容越来越多，会逐渐明显地影响性能。
+//所以如果你把v-for的内容单独封装为一个组件，由于diff只在当前组件,不会diff子组件，scroll-view更新时就不会导致v-for这部分内容的diff
+
+import { getCurrentInstance } from "vue";
+const {
+  setQueryTowxmlNodeFn,
+} = require("../../wxcomponents/towxml/globalCb");
+export default {
+  props: ["messages"],
+  emits: ["finish"],
+  setup(props, { emit }) {
+    const questionType = 0; //问题
+    const answerType = 1; //答案
+    const speed = 10;
+    function finish(e) {
+      emit("finish", e);
+    }
+    //为什么要麻烦你导入setQueryTowxmlNodeFn函数，并进行赋值，因为我为了节约内存，做虚拟显示，即只加载当前屏幕上下几屏的内容，必须要获得每一个towxml的top位置，同时为了性能和其他方面的考虑，我需要一次性获取所有towxml组件的top位置
+    //由于微信小程序的限制，createSelectorQuery只能查询到当前组件内的dom标签，不能查询父组件中或者页面上的标签，我无法在一个towmxl组件实例中获取其他towmxl实例对应的位置，所以不得不需要你的帮助
+    //你就保持这个函数不变（即使用selectAll查询towxml的父标签）我这里是用了一个view包裹了towxml,并设置了一个类名叫做answer-item，不要给这个view设什么padding之类的，最好保证这个view的高度和towxml的高度一致
+    //如果你没有设置这个函数，那么就没有虚拟显示功能，即显示所有内容，这样的话内存会慢慢撑爆而导致发热闪退
+    const instance = getCurrentInstance();
+    setQueryTowxmlNodeFn.value = (fn) => {
+      const query = uni.createSelectorQuery().in(instance);
+      query
+        .selectAll(".answer-item")
+        .boundingClientRect((towxmlNodes) => {
+          // console.log("页面上所有的towmxlNodes的值：",towxmlNodes)
+          fn(towxmlNodes);
+        })
+        .exec();
+    };
+    console.log("props.messages的值：", props.messages);
+    return {
+      questionType,
+      answerType,
+      speed,
+      finish,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./chat.scss";
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,105 +1,213 @@
 <template>
   <view class="page">
-    <scroll-view class="page-scroll" scroll-y :scroll-with-animation="true" :scroll-into-view="scrollIntoViewId"
-      scroll-style="none">
-      <view class="scroll-container">
-        <view :class="['item']">
-          <view class="answer-item">
-            <towxml :mdText="mdText" :speed="speed" :isFinish="isFinish" @finish="finish" v-if="isShow" />
-          </view>
-        </view>
+    <scroll-view
+      class="chat-scroll"
+      scroll-y
+      :scroll-with-animation="true"
+      scroll-style="none"
+      @scroll="onScroll"
+      @touchstart="onTouchStart"
+      :scroll-into-view="scrollIntoViewId"
+    >
+      <view class="chat-body">
+        <Chat :messages="messages" @finish="finish" />
         <view class="anchor" id="scroll-anchor"></view>
       </view>
     </scroll-view>
     <view class="input-container">
-      <input class="input" placeholder="请输入markdown文本地址" @confirm="sendQuestion" v-model="inputText" />
+      <input
+        class="input"
+        placeholder="请输入markdown文本url地址"
+        @confirm="sendQuestion"
+        v-model="inputText"
+      />
+      <button class="pause-button" @click="opClick">
+        {{ isTyping ? "终止" : "发送" }}
+      </button>
+    </view>
+    <view
+      :class="['arrow', showArrow ? 'arrow-show' : 'arrow-hide']"
+      v-if="showArrow"
+      @click="scrollToBottomAndResumeAutoScroll"
+    >
+      <text>↓</text>
     </view>
   </view>
 </template>
 
 <script>
 import { ref } from "vue";
+import { Chat } from "./chat.vue";
+const {
+  setMdText,
+  setStreamFinish,
+  stopImmediatelyCb,
+  scrollCb,
+} = require("../../wxcomponents/towxml/globalCb");
 
 export default {
+  components: { Chat },
   setup() {
-    const questionType = 0;
-    const answerType = 1;
+    const questionType = 0; //问题
+    const answerType = 1; //答案
     const inputText = ref("");
-    const scrollIntoViewId = ref("");
     let content = "";
-    const isFinish = ref(false);
-    const mdText = { text: "" };
+    let mdText = ""; //用来记录本次回答，流式接口累积返回的文本
     let timer = undefined;
-    const speed = 10;
     let scroller = 0;
-    const isShow = ref(false);
+    let curTowxmlId = ""; //记录当前正在打字的towxml组件的id
+    const messages = ref([]);
+    let autoScroll = true;
+    const showArrow = ref(false);
+    const screenHeight = uni.getSystemInfoSync().windowHeight;
+    const isTyping = ref(false);
+    const scrollIntoViewId = ref("");
 
     async function sendQuestion() {
-      isFinish.value = false
-      isShow.value = false;
-      mdText.text = "";
-      if (timer) {
-        clearInterval(timer);
+      if (!inputText.value) {
+        return;
       }
-      setTimeout(() => {
-        isShow.value = true;
-        uni.request({
-          url: inputText.value,
-          // url: `http://110.41.9.23/static/video-embed.md`,
-          // url: `http://110.41.9.23/static/md-editor-vue3.md`,
-          // url: `https://zxx-wwj-oss.oss-cn-shenzhen.aliyuncs.com/schChoose/article/d338e6c9-dc59-45d1-8482-5ea21d05f449/923e9f20-46da-4026-844b-f6a2c14ec0eb.md`,
-          // url: `https://zxx-wwj-oss.oss-cn-shenzhen.aliyuncs.com/schChoose/article/4d711758-074e-4be8-b280-77cc51719248/08c54e75-144f-426a-ba38-eb91cf464846.md`,
-          encoding: "utf-8",
-          success: (res) => {
-            generateStreamData(res.data)
-            inputText.value = "";
-          },
-          fail: (e) => {
-            generateStreamData("文件访问错误！")
-            inputText.value = "";
-          },
-        });
-      }, 1200);
+      //有可能呢上次的答案还没打字完，你又发送了新的问题，那么做一下重置操作
+      isTyping.value = false
+      setStreamFinish(curTowxmlId);
+      stopImmediatelyCb(curTowxmlId);
+
+      messages.value.push({
+        id: new Date().getTime(),
+        content: inputText.value,
+        type: questionType,
+      });
+      messages.value = [...messages.value];
+      uni.request({
+        url: inputText.value,
+        // url: `http://110.41.9.23/static/video-embed.md`,
+        // url: `http://110.41.9.23/static/md-editor-vue3.md`,
+        // url: `https://zxx-wwj-oss.oss-cn-shenzhen.aliyuncs.com/schChoose/article/d338e6c9-dc59-45d1-8482-5ea21d05f449/923e9f20-46da-4026-844b-f6a2c14ec0eb.md`,
+        // url: `https://zxx-wwj-oss.oss-cn-shenzhen.aliyuncs.com/schChoose/article/4d711758-074e-4be8-b280-77cc51719248/08c54e75-144f-426a-ba38-eb91cf464846.md`,
+        encoding: "utf-8",
+        success: (res) => {
+          //记录当前正在打字的towxml组件实例的id,id一定要唯一，因为每个towxml组件都有对应的全局数据，以id为索引，id重复会导致数据使用错乱
+          curTowxmlId = new Date().getTime();
+          messages.value.push({
+            id: curTowxmlId,
+            type: answerType,
+          });
+          messages.value = [...messages.value];
+          // 由于我没有大模型的流式接口，所以这里发给网络请求获取markdown文本，再使用定时器模拟流式接口，你的话就根据你的实际接口情况进行更改
+          generateStreamData(res.data);
+          inputText.value = "";
+        },
+        fail: (e) => {
+          curTowxmlId = new Date().getTime();
+          messages.value.push({
+            id: curTowxmlId,
+            type: answerType,
+          });
+          messages.value = [...messages.value];
+          generateStreamData("文件访问错误！");
+          inputText.value = "";
+        },
+      });
     }
 
+    //模拟流式接口
     function generateStreamData(text) {
+      isTyping.value = true;
+      //重置当次流式接口对应的文本
+      mdText = "";
       content = text;
       let c = 0;
-      //模拟流式接口
+      //使用定时器模拟流式接口
       timer = setInterval(() => {
-        if (scroller % 100 == 0) {
-          scrollIntoViewId.value = "";
-          function scrollRender() {
-            scrollIntoViewId.value = "scroll-anchor";
-          }
-          setTimeout(scrollRender, 0);
+        //每60*3毫秒将滚动条滚动到底部
+        if (scroller % 60 == 0 && autoScroll) {
+          scrollToBottom();
         }
         scroller++;
+        //流式接口结束
         if (c >= content.length) {
-          isFinish.value = true;
+          //通知towxml组件，流式接口结束，即本次回答所对应的所有markdown文本都已经拼接好了
+          //因为towmxl组件判断打字结的条件是：1.你的流式接口已经结束，即不在产生新的文本  2.打字的字符已经超过了文本字符   必须同时满足这两个条件，才能说明打字结束
+          // 你需要做的就是调用 setStreamFinish 函数通知到底层 towxml 组件流式接口结束，函数的参数是当前正在打字的 towxml 组件的 id
+          setStreamFinish(curTowxmlId);
           return;
         }
-        mdText.text = mdText.text + content[c];
+        //累积流式接口返回的文本，我这里模拟流式接口，所以是一个个字符取的，你当前流式接口返回多少字符，就全部拼接上即可
+        mdText = mdText + content[c];
+        //调用setMdText函数将最新的文本通知到底层towxml组件，函数的参数是当前正在打字的towxml组件的id，也就是<towxml :towxmlId="msgItem.id" :speed="speed" @finish="finish" />中，这个towxmlId
+        setMdText(curTowxmlId, mdText);
         c++;
       }, 3);
     }
 
+    //滚动到底部
+    function scrollToBottom() {
+      scrollIntoViewId.value = "";
+      function scrollRender() {
+        scrollIntoViewId.value = "scroll-anchor";
+      }
+      setTimeout(scrollRender, 0);
+    }
+
+    //当前回答的打字结束，finsih会被回调，你可以做一些关掉滚动定时器等善后操作
     function finish(e) {
       console.log(e);
       clearInterval(timer);
+      isTyping.value = false;
+    }
+
+    const onTouchStart = () => {
+      //只有正在打字的时候，用户滑动一下出现下滑箭头
+      if (!isTyping.value) {
+        return;
+      }
+      uni
+        .createSelectorQuery()
+        .select(".chat-scroll")
+        .scrollOffset((res) => {
+          const scrollHeight = res.scrollHeight;
+          if (scrollHeight > screenHeight) {
+            autoScroll = false;
+            showArrow.value = true;
+          }
+        })
+        .exec();
+    };
+
+    //点击箭头下滑到底部，并开启自动滚动更新
+    function scrollToBottomAndResumeAutoScroll() {
+      autoScroll = true;
+      showArrow.value = false;
+      scrollToBottom();
+    }
+
+    function opClick() {
+      if (isTyping.value) {
+        isTyping.value = false
+        stopImmediatelyCb(curTowxmlId);
+      } else {
+        sendQuestion();
+      }
+    }
+
+    // 滚动事件回调，记得在滚动事件中回调 scrollCb，并把参数进行传入，我会根据这个参数判断滚动方向，这样 towxml 组件内部才知道你滚动了，就会帮你进行虚拟显示
+    // 如果你没有回调 scrollCb，towxml 组件内部就不会进行虚拟显示，随着程序的持续运行，对话次数的增多，页面的 dom 节点会越来越多，导致 1. 内存占用过大而发烫闪退  2. 随着内存占用过大，垃圾回收频繁，占据了很多运行时间，导致后期的打字速度受影响
+    function onScroll(e) {
+      scrollCb(e);
     }
 
     return {
-      questionType,
-      answerType,
       inputText,
       sendQuestion,
-      scrollIntoViewId,
-      isFinish,
-      mdText,
       finish,
-      speed,
-      isShow,
+      messages,
+      onTouchStart,
+      scrollToBottomAndResumeAutoScroll,
+      showArrow,
+      opClick,
+      isTyping,
+      scrollIntoViewId,
+      onScroll,
     };
   },
 };
